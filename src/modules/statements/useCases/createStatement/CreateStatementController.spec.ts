65db2c91881ae121ff5af1ef2c8df80f615abcb1
leo-jesus <llxdgb1@gmail.com>
2021-04-16 14:29:00 -0300

    add create statement controller test

---
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -0,0 +1,63 @@
+import request from "supertest";
+import { Connection, createConnection } from "typeorm";
+import { app } from "../../../../app";
+import { ICreateUserDTO } from "../../../users/useCases/createUser/ICreateUserDTO";
+import { ICreateStatementDTO } from "./ICreateStatementDTO";
+
+describe("Create statement controller", () => {
+  let database: Connection;
+
+  beforeAll(async () => {
+    database = await createConnection();
+    await database.runMigrations();
+  });
+
+  enum OperationType {
+    DEPOSIT = "deposit",
+    WITHDRAW = "withdraw",
+  }
+
+  afterAll(async () => {
+    await database.dropDatabase();
+    await database.close();
+  });
+  it("should be able do create a new deposit statment", async () => {
+    const user: ICreateUserDTO = {
+      name: "User test",
+      email: "test@mail.com",
+      password: "12345",
+    };
+    await request(app).post("/api/v1/users").send(user);
+
+    const createUser = await request(app).post("/api/v1/sessions").send({
+      email: user.email,
+      password: user.password,
+    });
+    const { sessionUser, token } = createUser.body;
+    const response = await request(app)
+      .post("/api/v1/statements/deposit")
+      .send({
+        amount: 100,
+        description: "deposit test",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(201);
+  });
+
+  it("Shouldn't be able to create a  statement to invalid token", async () => {
+    const response = await request(app)
+      .post("/api/v1/statements/deposit")
+      .send({
+        amount: 100,
+        description: "deposit test",
+      })
+      .set({
+        Authorization: `Bearer invalid-token`,
+      });
+
+    expect(response.status).toBe(401);
+  });
+});
